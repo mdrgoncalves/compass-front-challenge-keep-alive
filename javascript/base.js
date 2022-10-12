9afff4951d22fdc6d4324b2c2bc7b55e9ae7fe8b
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-11 22:03:44 -0300

    feat: animating icon

---
+++ javascript/base.js
@@ -0,0 +1,7 @@
+export const elements = {
+    username: document.querySelector('#username'),
+    usernameIcon: document.querySelector('.username__icon'),
+    password: document.querySelector('#password'),
+    passwordIcon: document.querySelector('.password__icon'),
+    submit: document.querySelector('.authentication__button'),
+}
