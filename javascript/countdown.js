61c9fdab1ebf15fb30e43e0a5c98ba7055e7052f
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-13 12:00:57 -0300

    fix: countdown redirect

--- javascript/countdown.js
+++ javascript/countdown.js
@@ -1,11 +1,19 @@
 import { elements } from './baseHome.js';
 
-(function countdown() {
+function countdown() {
 
     let startTimer = 600;
 
     setInterval(() => {
+
         startTimer--;
         elements.counter.textContent = startTimer;
+
+        if(startTimer === 0) {
+            window.location = '../index.html';
+        }
+
     }, 1000);
-})();
+};
+
+window.addEventListener('load', countdown);
