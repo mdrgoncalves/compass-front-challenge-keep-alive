924810fcdf3e3dd977cb0eea6a99ddd48dbc881d
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-13 21:08:36 -0300

    style: fix weather icon size

--- javascript/setWeather.js
+++ javascript/setWeather.js
@@ -7,6 +7,13 @@ export function setWeather(city) {
     fetch(urlWeather).then((response) => response.json())
         .then((data) => {
             elements.temperature.textContent = `${Math.round(data.current.temp_c)}°C`;
+
+            //Change weather icon size if clear
+            const weatherIconId = data.current.condition.icon.split('/').pop().split('.').shift();
+            if(weatherIconId === '113') {
+                elements.weatherIcon.classList.add('background-size-js');
+            }
+
             elements.weatherIcon.style.backgroundImage = `url(${data.current.condition.icon})`;
         })
         .catch((error) => {
